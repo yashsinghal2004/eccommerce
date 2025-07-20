8ff17c0cdd3e4c90d503a1797c6e1edf51035056
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2025-07-21 02:00:37 +0530

    updated

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { createContext, useContext, useState } from "react";
+
+export const DataContext = createContext(null);
+
+export const DataProvider = ({ children }) => {
+    const [data, setData] = useState()
+
+    // fetching all products from api
+    const fetchAllProducts = async () => {
+        try {
+           const res = await axios.get('https://fakestoreapi.in/api/products?limit=150')
+           console.log(res);
+           const productsData = res.data.products
+           setData(productsData)
+           
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    const getUniqueCategory = (data, property) =>{
+        let newVal = data?.map((curElem) =>{
+            return curElem[property]
+        })
+        newVal = ["All",...new Set(newVal)]
+        return newVal
+      }
+    
+      const categoryOnlyData = getUniqueCategory(data, "category")
+      const brandOnlyData = getUniqueCategory(data, "brand")
+    return <DataContext.Provider value={{ data, setData,fetchAllProducts, categoryOnlyData, brandOnlyData }}>
+        {children}
+    </DataContext.Provider>
+}
+
+export const getData = ()=> useContext(DataContext)
